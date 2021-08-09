6d93eb01048dca84bf5020cfab34e187f2e4ef30
maxminoS <maxminos.1@gmail.com>
2021-08-09 20:19:07 +0700

    Clean script
    - Rename to use dots instead of colons
    - Fix when ffprobe sort of misbehaves
    - Use variables for cleaner separation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # oolong
+Requirement: `ffmpeg`
 
-A simple shell script to rename all the files in the directory to include their lengths (in hh:mm:ss).
+A simple shell script to rename all the files in the directory to include their lengths (in hh.mm.ss).
 
 ## Example
 
@@ -8,7 +9,4 @@ From:
 `Harry Potter and the Sorcerer's Stone (Audiobook).mp3`
 
 into:
-`Harry Potter and the Sorcerer's Stone (Audiobook) [08:18:02].mp3`
-
-
-Requirement: `ffmpeg`
+`Harry Potter and the Sorcerer's Stone (Audiobook) [08.18.02].mp3`
--- oolong.sh
+++ oolong.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 for file in *; do
-	if [ -f "$file" ]; then
-		mv "$file" "${file%.*} [$(ffmpeg -i "$file" 2>&1 | grep Duration | awk '{print $2}' | cut -d '.' -f 1)].${file##*.}"
-	fi
+  if [ -f "$file" ]; then
+      DURATION=$(ffmpeg -i "$file" 2>&1 | grep Duration | awk '{print $2}' | cut -d. -f1 | tr : .)
+      FIXED_HOUR=$(echo "$DURATION" | cut -d. -f1 | tail -c 3)
+      FIXED_MINUTE=$(echo "$DURATION" | cut -d. -f2)
+      FIXED_SECOND=$(echo "$DURATION" | cut -d. -f3)
+      FIXED_DURATION="$FIXED_HOUR.$FIXED_MINUTE.$FIXED_SECOND"
+      mv "$file" "${file%.*} [$FIXED_DURATION].${file##*.}"
+  fi
 done
-echo "Duration of all files shown"
+echo "Added file durations"
